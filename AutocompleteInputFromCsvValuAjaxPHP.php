9b43c61b0842e8b2e8a9506ea58832788458a9cb
alshahirar <48322349+alshahirar@users.noreply.github.com>
2019-12-03 17:19:29 +0600

    Update AutocompleteInputFromCsvValuAjaxPHP.php

--- AutocompleteInputFromCsvValuAjaxPHP.php
+++ AutocompleteInputFromCsvValuAjaxPHP.php
@@ -17,7 +17,7 @@
   <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
   <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
   
-<label for="autocomplete">Select a programming language: </label>
+<label for="autocomplete">Select Email: </label>
 <input id="autocomplete">
 
 
