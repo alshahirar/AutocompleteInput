681790659266b66826f85c9a4dc35b261235423e
Shahriar <shahriarnes526@gmail.com>
2019-12-03 17:15:30 +0600

    fullcode

---
+++ AutocompleteInputFromCsvValuAjaxPHP.php
@@ -0,0 +1,37 @@
+<?php
+		$file = fopen('output.csv', 'r') or die('Unable to open file!');
+		
+		while(($row = fgetcsv($file)) !== false){
+			
+				$test = $row[4];
+				$jsonArray[]= $row[4];	
+}	
+		$test1 = $jsonArray;
+		$trimmed_array = array_map('trim', $test1);
+		echo json_encode($trimmed_array);
+
+		fclose($file);
+				
+?>
+  <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+  
+<label for="autocomplete">Select a programming language: </label>
+<input id="autocomplete">
+
+
+<script>
+$( "#autocomplete" ).autocomplete({
+  source: function( request, response ) {
+	  
+					
+		var tags = <?php echo json_encode($trimmed_array);?>;
+          var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( request.term ), "i" );
+          response( $.grep( tags, function( item ){
+              return matcher.test( item );
+          }) );
+      }
+});
+</script>
+
